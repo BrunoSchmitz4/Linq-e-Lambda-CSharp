b17f8c145b38f5bcdd5476437653de4f4c6fc4c7
Bruno Silva <brunosch250@gmail.com>
2023-07-12 11:55:26 -0300

    update

--- LinqFirstApp/Program.cs
+++ LinqFirstApp/Program.cs
@@ -12,9 +12,66 @@ namespace LinqFirstApp
     {
         static void Main(string[] args)
         {
-            LinqWithLambda();
+            AlgebraRelacionalWithSQL();
         }
 
+        static void AlgebraRelacionalWithSQL()
+        {
+            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+            Category c3 = new Category() { Id = 3, Name = "Electronics", Tier = 1 };
+
+            List<Product> products = new List<Product>() {
+                new Product() { Id = 1, Name = "Computer", Price = 1100.0, Category = c2},
+                new Product() { Id = 2, Name = "Hammer", Price = 90.0, Category = c1},
+                new Product() { Id = 3, Name = "TV", Price = 1700.0, Category = c3},
+                new Product() { Id = 4, Name = "Notebook", Price = 1300.0, Category = c2},
+                new Product() { Id = 5, Name = "Saw", Price = 80.0, Category = c1},
+                new Product() { Id = 6, Name = "Tablet", Price = 700.0, Category = c2},
+                new Product() { Id = 7, Name = "Camera", Price = 700.0, Category = c3},
+                new Product() { Id = 8, Name = "Printer", Price = 350.0, Category = c3},
+                new Product() { Id = 9, Name = "MacBook", Price = 1800.0, Category = c2},
+                new Product() { Id = 10, Name = "Sound Bar", Price = 700.0, Category = c3},
+                new Product() { Id = 11, Name = "Level", Price = 70.0, Category = c1},
+            };
+
+            var r1 = from p in products where p.Category.Tier == 1 && p.Price < 900.00 select p;
+            Print("\nAll the Tier 1 products that cost less than: R$ 900.00: ", r1);
+
+            var r2 = from p in products where p.Category.Name == "Tools" select p.Name;
+            Print("\nEvery Products in Tool's category: ", r2);
+
+            var r3 = from p in products where p.Name.StartsWith("C") select p;
+            Print("\nEvery Products starting their name with 'C': ", r3);
+
+            var r4 = from p in products where p.Category.Tier == 1 orderby p.Name orderby p.Price select p;
+            Print("\nOrding products of Tier 1 by theirs price, then their name: ", r4);
+
+            var r5 = (from p in r4 select p).Skip(2).Take(4);
+            Print("\nAll the tier 1 products ordered by their price, then by their name. It skip 2 and Take 4: ", r5);
+
+            var r6 = (from p in products select p).FirstOrDefault();
+            Console.WriteLine("\nThe first one or default: " + r6);
+            var r7 = (from p in products where p.Price > 3000.00 select p).FirstOrDefault();
+            if (r7 == null)
+            {
+                Console.WriteLine("\nThe first one or default (null): Null");
+            }
+            else
+            {
+                Console.WriteLine("\nThe first one or default (null): " + r7);
+            }
+
+            var r16 = from p in products group p by p.Category;
+            foreach(var group in r16)
+            {
+                Console.WriteLine($"Category: {group.Key.Name}: ");
+                foreach(Product p in group)
+                {
+                    Console.WriteLine($"{p}\n");
+                }
+            }
+        }
         static void LinqWithLambda()
         {
             Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
